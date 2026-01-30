bc2b3a324e9d9f42cd1217cc8dca925ea25810a0
bernard-wagner <bernard-wagner@users.noreply.github.com>
2026-01-30 11:04:53 +0200

    aliases

---
+++ crates/chisel/bin/chisel_alias.rs
@@ -0,0 +1,14 @@
+// Alias binary for backwards compatibility and tests.
+// This is identical to arbos-chisel.
+
+use chisel::args::run;
+
+#[global_allocator]
+static ALLOC: foundry_cli::utils::Allocator = foundry_cli::utils::new_allocator();
+
+fn main() {
+    if let Err(err) = run() {
+        let _ = foundry_common::sh_err!("{err:?}");
+        std::process::exit(1);
+    }
+}
